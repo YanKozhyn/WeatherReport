0b84d4c5d5cbb61b5edd56b3b56046ad0b568e59
Yan <jan.kozhyn@gmail.com>
2022-12-14 14:14:01 +0200

    trying to fix bug

--- CloudWeather.DataLoader/build_and_push_ecr_image.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-aws ecr get-login-password --region us-east-1 --profile weather-ecr-agent | docker login --username AWS --password-stdin 155209979941.dkr.ecr.us-east-1.amazonaws.com
-docker build -f ./Dockerfile -t cloud-weather-data-loader:latest .
-docker tag cloud-weather-data-loader:latest 155209979941.dkr.ecr.us-east-1.amzonaws.com/cloud-weather-data-loader:latest
-docker push 155209979941.dkr.ecr.us-east-1.amazonaws.com/cloud-weather-data-loader:latest
